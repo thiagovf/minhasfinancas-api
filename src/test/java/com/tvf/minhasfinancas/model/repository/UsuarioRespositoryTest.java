2aae6f4ce85c9e4bf9f2a5eb420ae3174c260c90
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-13 21:31:35 -0300

    adicionando teste

---
+++ src/test/java/com/tvf/minhasfinancas/model/repository/UsuarioRespositoryTest.java
@@ -0,0 +1,31 @@
+package com.tvf.minhasfinancas.model.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tvf.minhasfinancas.model.entity.Usuario;
+
+/**
+ * Teste de integração.
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UsuarioRespositoryTest {
+
+	@Autowired
+	UsuarioRepository repository;
+
+	@Test
+	public void deveVerificarAExistenciaDeUmEmail() {
+		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
+		repository.save(usuario);
+		
+		boolean result = repository.existsByEmail("usuario@email.com");
+		
+		Assertions.assertThat(result).isTrue();
+	}
+}
