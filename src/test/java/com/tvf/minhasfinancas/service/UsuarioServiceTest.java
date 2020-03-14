ca18ebefe96abf453cc9d6bb3d964ae3899d79f5
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-14 15:19:51 -0300

    adicionando novos testes

---
+++ src/test/java/com/tvf/minhasfinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,42 @@
+package com.tvf.minhasfinancas.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tvf.minhasfinancas.model.entity.Usuario;
+import com.tvf.minhasfinancas.model.repository.UsuarioRepository;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UsuarioServiceTest {
+
+	@Autowired
+	UsuarioService service;
+	
+	@Autowired
+	UsuarioRepository repository;
+	
+	@Test(expected = Test.None.class)
+	public void deveValidarEmail() {
+		//cenário
+		repository.deleteAll();
+		
+		//acao
+		service.validarEmail("email@email.com");
+	}
+	
+	@Test
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		//cenario
+		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
+		repository.save(usuario);
+		
+		//acao
+		service.validarEmail("email@email.com");
+	}
+}
