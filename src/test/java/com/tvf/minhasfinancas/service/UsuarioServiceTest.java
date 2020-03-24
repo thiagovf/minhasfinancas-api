fb958c929ef7a8a60092603ed7e158eac9d5f7df
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-24 11:21:32 -0300

    evoluindo alguns testes

--- src/test/java/com/tvf/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/tvf/minhasfinancas/service/UsuarioServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,12 +21,13 @@ public class UsuarioServiceTest {
 	UsuarioService service;
 	
 //	@Autowired
+	@MockBean
 	UsuarioRepository repository;
 	
 	
 	@Before
 	public void setUp() {
-		repository = Mockito.mock(UsuarioRepository.class);
+//		repository = Mockito.mock(UsuarioRepository.class);
 		service = new UsuarioServiceImpl(repository);
 	}
 	
