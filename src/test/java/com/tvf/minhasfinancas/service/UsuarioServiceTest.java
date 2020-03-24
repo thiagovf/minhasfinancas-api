9a289be5c2bf182810af8689b3bcf7766951983a
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-24 16:30:40 -0300

    evoluindo alguns testes

--- src/test/java/com/tvf/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/tvf/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,5 +1,8 @@
 package com.tvf.minhasfinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +11,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.tvf.minhasfinancas.exception.ErroAutenticacao;
 import com.tvf.minhasfinancas.exception.RegraNegocioException;
+import com.tvf.minhasfinancas.model.entity.Usuario;
 import com.tvf.minhasfinancas.model.repository.UsuarioRepository;
 import com.tvf.minhasfinancas.service.impl.UsuarioServiceImpl;
 
@@ -29,6 +34,42 @@ public class UsuarioServiceTest {
 	public void setUp() {
 //		repository = Mockito.mock(UsuarioRepository.class);
 		service = new UsuarioServiceImpl(repository);
+	}
+	
+	@Test(expected = Test.None.class)
+	public void deveAutenticarUmUsuarioComSucesso() {
+		//cenario
+		String email = "email@email.com";
+		String senha = "senha";
+		
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Usuario result = service.autenticar(email, senha);
+		
+		//verificacao
+		Assertions.assertThat(result).isNotNull();
+	}
+	
+	@Test(expected = ErroAutenticacao.class)
+	public void deveLancarErroQuandoNaoEncontrarUsuarioCadastradoComOEmailInformado() {
+		//cenario
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+		
+		//acao
+		service.autenticar("email@email.com", "senha");
+	}
+	
+	@Test(expected = ErroAutenticacao.class)
+	public void deveLancarErroQuandoSenhaNaoBater() {
+		//cenario
+		String senha = "senha";
+		Usuario usuario = Usuario.builder().email("email@email.com").senha(senha).build();
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+		
+		//acao
+		service.autenticar("email@email.com", "123");
 	}
 	
 	@Test(expected = Test.None.class)
