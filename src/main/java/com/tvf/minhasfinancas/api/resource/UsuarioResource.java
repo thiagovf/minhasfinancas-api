397621fb37ee2be2d6f66fe4cc1a9a28bc6d6eb5
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-25 17:42:19 -0300

    criando método de autenticar e salvar no controller

--- src/main/java/com/tvf/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/tvf/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,13 +1,49 @@
 package com.tvf.minhasfinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tvf.minhasfinancas.api.dto.UsuarioAutenticarDTO;
+import com.tvf.minhasfinancas.api.dto.UsuarioDTO;
+import com.tvf.minhasfinancas.exception.ErroAutenticacao;
+import com.tvf.minhasfinancas.exception.RegraNegocioException;
+import com.tvf.minhasfinancas.model.entity.Usuario;
+import com.tvf.minhasfinancas.service.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
-	@GetMapping("/")
-	public String helloWorld() {
-		return "hello world";
+	private UsuarioService service;
+
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+
+	@PostMapping("/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioAutenticarDTO dto) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado);
+		} catch (ErroAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+
+		Usuario usuario = Usuario.builder().email(dto.getEmail()).nome(dto.getNome()).senha(dto.getSenha()).build();
+
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
