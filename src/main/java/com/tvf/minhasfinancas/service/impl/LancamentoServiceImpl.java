31fd41db32899d4277806bf21c7e2a00c59abe66
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-26 19:47:09 -0300

    adicionando implementações do métodos

--- src/main/java/com/tvf/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/tvf/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,41 +1,60 @@
 package com.tvf.minhasfinancas.service.impl;
 
 import java.util.List;
+import java.util.Objects;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tvf.minhasfinancas.model.entity.Lancamento;
 import com.tvf.minhasfinancas.model.enums.StatusLancamento;
+import com.tvf.minhasfinancas.model.repository.LancamentoRepository;
 import com.tvf.minhasfinancas.service.LancamentoService;
 
+@Service
 public class LancamentoServiceImpl implements LancamentoService {
 
+	private LancamentoRepository repository;
+
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+
 	@Override
+	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public Lancamento atualizar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		Objects.requireNonNull(lancamento.getId());
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public void deletar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		
+		Objects.requireNonNull(lancamento.getId());
+		repository.delete(lancamento);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-		// TODO Auto-generated method stub
-		return null;
+		Example example = Example.of(lancamentoFiltro,
+				ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+		return repository.findAll(example);
 	}
 
 	@Override
 	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
-		// TODO Auto-generated method stub
-		
+		lancamento.setStatus(status);
+		atualizar(lancamento);
 	}
 
 }
