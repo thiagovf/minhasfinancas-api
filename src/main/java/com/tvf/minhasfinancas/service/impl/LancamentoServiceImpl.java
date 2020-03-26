7804a65f351c9aafcab9e05359fee550bb19f44a
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-26 19:38:41 -0300

    criando serviço de lançamentos

---
+++ src/main/java/com/tvf/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,41 @@
+package com.tvf.minhasfinancas.service.impl;
+
+import java.util.List;
+
+import com.tvf.minhasfinancas.model.entity.Lancamento;
+import com.tvf.minhasfinancas.model.enums.StatusLancamento;
+import com.tvf.minhasfinancas.service.LancamentoService;
+
+public class LancamentoServiceImpl implements LancamentoService {
+
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
