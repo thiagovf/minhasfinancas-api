04b8601443842f1222dd403faee6c6bb6525d8e7
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-28 10:28:29 -0300

    adicionando método de busca e de delete lancamentos

--- src/main/java/com/tvf/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/tvf/minhasfinancas/api/resource/LancamentoResource.java
@@ -1,12 +1,18 @@
 package com.tvf.minhasfinancas.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tvf.minhasfinancas.api.dto.LancamentoDTO;
@@ -25,8 +31,32 @@ public class LancamentoResource {
 	private LancamentoService service;
 	private UsuarioService usuarioService;
 
-	public LancamentoResource(LancamentoService service) {
+	public LancamentoResource(LancamentoService service, UsuarioService usuarioService) {
 		this.service = service;
+		this.usuarioService = usuarioService;
+	}
+
+	@GetMapping
+	public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam(value = "usuario", required = true) Long idUsuario) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+
+		if (usuario.isPresent()) {
+			return ResponseEntity.badRequest()
+					.body("Não foi possível realizar a consulta. Usuário não encontrado para o ID informado.");
+		} else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
 	}
 
 	@PostMapping
@@ -48,9 +78,17 @@ public class LancamentoResource {
 				lancamento.setId(entity.getId());
 				service.atualizar(lancamento);
 				return ResponseEntity.ok(lancamento);
-			} catch (Exception e) {
+			} catch (RegraNegocioException e) {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
+		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
+	}
+
+	@DeleteMapping("{id}")
+	private ResponseEntity deletar(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
 	}
 
