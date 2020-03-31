f8ef7277d594591cea7b6bab3231a5a8a9af5874
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-03-31 19:38:11 -0300

    pequenas correções

--- src/main/java/com/tvf/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/tvf/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.tvf.minhasfinancas.model.entity.Lancamento;
+import com.tvf.minhasfinancas.model.enums.TipoLancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-	@Query(value = " select sum(l.valor()) from lancamento l join l.usuario u "
+	@Query(value = " select sum(l.valor) from Lancamento l join l.usuario u "
 			+ "where u.id = :idUsuario and l.tipo = :tipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") String tipo);
+	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
 }
